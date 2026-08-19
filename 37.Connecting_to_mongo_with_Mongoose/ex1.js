856137753c847d5cf42cf2afc9a01f3857d6cf8d
Anikaith <anikaithkamala07@gmail.com>
2026-08-19 13:17:23 +0530

    routes

--- 37.Connecting_to_mongo_with_Mongoose/ex1.js
+++ 37.Connecting_to_mongo_with_Mongoose/ex1.js
@@ -55,3 +55,76 @@ const userSchema=new mongoose.Schema({
 //3.create the model
 const User=new mongoose.model("User",userSchema);
 
+//4. create a user inside a route
+
+const app=express();
+
+app.listen(3000,()=>{
+    console.log("Listening on port 3000");
+});
+
+app.post("/users",async (req,res)=>{
+    try{
+    const user=await User.create(req.body);
+    res.json(user);
+    }
+    catch(error){
+        res.status(400).json({error: error.message});
+    }
+});
+
+//5.Insert many users
+app.post("/users/many",async (req,res)=>{
+    try{
+        const user=await User.insertMany(req.body);
+        res.json(user);
+    }
+    catch(error){
+        res.status(400).json({error:error.message});
+    }
+});
+
+//6.find all users
+app.get("/users",async (req,res)=>{
+    try{
+        const users=await User.find();
+        res.json(users);
+    }
+    catch(error){
+        res.status(400).json({error:error.message});
+    }
+});
+
+//7.Find one user
+app.get("/users/email/:email", async(req,res)=>{
+    try{
+        const user=await User.findOne({email:req.params.email});
+        res.json(user);
+    }
+    catch(error){
+        res.status(400).json({error:error.message});
+    }
+});
+
+//8.Find user by id
+app.get("/users/:id",async (req,res)=>{
+    try{
+        const user= await User.findById(req.params.id);
+        res.json(user);
+    }
+    catch(error){
+        res.status(400).json({error:error.message});
+    }
+});
+
+//9.query operators
+app.get("/user/adults",async (req,res)=>{
+    try{
+        const users=await User.find({age:{$gte:18}});
+        res.json(users);
+    }
+    catch(error){
+        res.status(400).json({error:error.message});
+    }
+});
+
